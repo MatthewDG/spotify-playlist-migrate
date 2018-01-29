9200c9d65b2dc567b32ea11202fbf57ca76353bb
Matt <mdg408@nyu.edu>
2018-01-29 00:05:04 -0500

    Exclude tracks deleted in Apple Music but still appear in the playlist

--- server/utils/xmlTojson.js
+++ server/utils/xmlTojson.js
@@ -1,5 +1,5 @@
 const xml2js = require('xml2js');
-const { map } = require('lodash');
+const { reduce, includes } = require('lodash');
 const fs = require('fs');
 
 const parser = new xml2js.Parser();
@@ -8,12 +8,18 @@ const xmlToJson = (data, callback) => {
     return parser.parseString(data,  (err, result) => {
         try {
           let trackArray = result.plist.dict[0].dict[0].dict;
-          let mappedTrackArray = map(trackArray, (track) => {
-            return {
-              title: track.string[0],
-              aritst: track.string[1],
+          
+          let mappedTrackArray = reduce(trackArray, (list, track) => {
+            if(includes(track.key, 'Name') && includes(track.key, 'Artist')){
+              list.push({
+                title: track.string[0],
+                aritst: track.string[1],
+              });
+              return list;
             }
-          });
+            
+            return list;
+          }, []);
           return callback(null, mappedTrackArray);
         }
         catch(e){
